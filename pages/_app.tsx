56c359a36ce491b075e2c52349de13ea08a84420
adithyalearnings <83404854+adithyalearnings@users.noreply.github.com>
2026-08-13 09:37:54 +0530

    Fix mobile viewport and load mobile layout overrides

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
+import Head from 'next/head';
 import '../styles/globals.css';
+import '../styles/mobile-fix.css';
 
 function MyApp(props: any) {
   const Page = props.Component;
-  return <Page {...props.pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+      </Head>
+      <Page {...props.pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
